d6861dc664b6602f7f03dc9b0bfd3fafaa972f2a
khanAston <122929455+khanAston@users.noreply.github.com>
2023-11-09 17:02:45 +0300

    Update ageCheck.hs
    Comment added by student

--- .github/workflows/ageCheck.hs
+++ .github/workflows/ageCheck.hs
@@ -1,3 +1,4 @@
+--Test comment added by student to haskell file
 checkAge age |age < 18  = putStrLn "Hello, Young One!"
              |age < 60  = putStrLn "Hello, Adult!"
              |otherwise = putStrLn "Hello, Pensioner!"
